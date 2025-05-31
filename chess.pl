a95d1aaae0c3a51f855ce179516b81f23d63c8c0
arya <aryaatarzadeh@gmail.com>
2025-05-31 20:40:06 +0800

    Added printing functionality

--- chess.pl
+++ chess.pl
@@ -49,6 +49,7 @@
 main :-
     randomize,         % Setting up the seed for random number generator
     init_board(InBoard),
+    print_board(InBoard), % TODO: DELETE THIS LINE BEFORE SUBMITTING!
     play(InBoard),          % Start playing
 	fail.
 main.
@@ -359,7 +360,111 @@ report_move(Color, Board, From_File-From_Rank, To_File-To_Rank, Rating) :-
 
 % TASK 1: REPLACE THE print_board PREDICATE BELOW WITH YOUR CODE
 print_board(Board) :-
-    write(Board), nl.
+    draw_board(8, 8, Board).
+
+% Base case. Prints the last row of the grid, including  file characters at the very bottom.
+draw_board(0, Col, Board) :- drawSymbol(' ', 1), % Print space before start of bottom line in grid.
+                      draw_up_bot_border(Col),
+                      nl,
+                      draw_pair. % Prints file characters (a, b, c, d ...)
+
+% Prints each row of the chess board (except the last, which is handled by the base case).
+draw_board(Line, Col, Board):- Line > 0, % Print space below "1" in grid
+                        drawSymbol(' ',1), % Print space before start of top line in grid
+                        draw_up_bot_border(Col), % Draw horizonta line grid.
+                        draw_content_line(Line, Col, Board), % Draw box sides. |    |
+                        Line1 is Line-1,
+                        draw_board(Line1, Col, Board).
+
+
+% Stands for - draw up bottom border. 
+% Base case. draw_up_bot_border() draws the horizontal line grids. +---+---+---+ 
+draw_up_bot_border(0):- drawSymbol('+', 1), nl.
+draw_up_bot_border(N):- N>0,
+                        write('+'),
+                        drawSymbol('-', 4),
+                        N1 is N-1,
+                        draw_up_bot_border(N1).
+
+
+% Draws line number then calls the draw_content_cell() function to draw content of grid in each grid spot.
+draw_content_line(Line, Col, Board):- drawSymbol(Line, 1),
+                               draw_content_cell(Line, Col, Board),
+                               nl.
+
+% Function draws content inside of each square. Prints abbreviated piece name and * if piece color is black. 
+draw_cell(Line, Col, Board):- pair(Name, Col), % Get file name of given column.
+                              mymember(piece(Name-Line, Color, Piece), Board), % Check if piece is still on the board...
+                              drawSymbol(' ', 1),
+                              (
+                                (Color == black, drawSymbol('*', 1)); % Print * to indicate piece is black.
+                                (Color == white, drawSymbol(' ', 1))  % Don't print anything for white pieces.
+                              ),
+                              pair(Piece, Cap), % find abbreviation of piece name.
+                              drawSymbol(Cap, 1),
+                              drawSymbol(' ', 1).
+
+% Function draws content inside of each square. Prints blank (4 spaces) when square has no pieces. 
+draw_cell(Line, Col, Board):- pair(Name, Col), % Get file name of given column.
+                              \+ (mymember(piece(Name-Line, Color, Piece), Board)), % \+ means negation. If piece is not on board...
+                              drawSymbol(' ', 4).
+
+% Draws left right grid lines and content of the given grid spot. 
+draw_content_cell(Line, 0, Board):- drawSymbol('|', 1).
+draw_content_cell(Line, C, Board):- C>0,
+                             drawSymbol('|', 1),
+                             % drawSymbol(' ', 4),
+                             draw_cell(Line, C, Board), % Draw actual content inside the square.
+                             C1 is C-1,
+                             draw_content_cell(Line, C1, Board).
+
+% Draws all File characters.
+draw_pair:- drawSymbol(' ', 4),
+            drawSymbol('a', 1),
+            drawSymbol(' ', 4),
+            drawSymbol('b', 1),
+            drawSymbol(' ', 4),
+            drawSymbol('c', 1),
+            drawSymbol(' ', 4),
+            drawSymbol('d', 1),
+            drawSymbol(' ', 4),
+            drawSymbol('e', 1),
+            drawSymbol(' ', 4),
+            drawSymbol('f', 1),
+            drawSymbol(' ', 4),
+            drawSymbol('g', 1),
+            drawSymbol(' ', 4),
+            drawSymbol('h', 1).
+
+
+% Links Rank with File. Line is usually used instead of Rank in the code.
+pair(a, 8).
+pair(b, 7).
+pair(c, 6).
+pair(d, 5).
+pair(e, 4).
+pair(f, 3).
+pair(g, 2).
+pair(h, 1).
+
+% Links piece name with abbreviation.
+pair(rook, r).
+pair(bishop, b).
+pair(king, k).
+pair(pawn, p).
+pair(queen, q).
+pair(night, n).
+
+
+%%  HELPER FUNCTION: DRAWING N NUMBER OF SYMBOLS  %%
+% Base case.
+drawSymbol(_, 0). 
+% Recursion Step.
+drawSymbol(Symbol, N) :- 
+    N>0,
+    write(Symbol),
+    N1 is N-1,
+    drawSymbol(Symbol, N1).
 
 % ------------------------------------------------------------------------------
 % YOUR CODE ENDS HERE
